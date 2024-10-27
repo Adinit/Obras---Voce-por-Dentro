5b655a4a31a2dd7db83c09a2953a2d62a01d80fa
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-10-27 23:59:13 +0000

    Adicionado link ao header

--- vc_por_dentro_front/src/components/Header.vue
+++ vc_por_dentro_front/src/components/Header.vue
@@ -2,7 +2,14 @@
 </script>
 
 <template>
-  <br-header title="Obras" subtitle="Você por dentro" is-sticky="true" has-search="true"
-    placeholder-search="Digite o texto da pesquisa" label-search="O que você procura?">
-  </br-header>
+
+<br-header
+  title="Obras - Você por dentro"
+  is-sticky="true"
+  has-search="true"
+  placeholder-search="Digite o texto da pesquisa"
+  label-search="Que obra você procura?"
+  has-title-link="true"
+  title-link-url="http://localhost:5173/"
+  ></br-header>
 </template>
